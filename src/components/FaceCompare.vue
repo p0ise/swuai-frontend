58b80981ed3d1a0ea8dc3aa29d4e2d55c78bac79
p0ise <changelf@163.com>
2024-03-28 17:16:51 +0800

    optimize face_comparison info text

--- src/components/FaceCompare.vue
+++ src/components/FaceCompare.vue
@@ -3,28 +3,29 @@
         <v-row justify="center">
             <v-col cols="12" md="8" lg="6">
                 <div class="text-h5 text-center mb-4">人脸比对</div>
-                <div class="my-4">
-                    <v-row align="center" justify="space-around">
-                        <v-col cols="12" sm="8" md="6">
-                            <!-- 为图片预览设置最小高度 -->
-                            <image-upload v-model="image1"></image-upload>
-                        </v-col>
-                        <v-col cols="12" sm="8" md="6">
-                            <image-upload v-model="image2"></image-upload>
-                        </v-col>
-                    </v-row>
-                    <v-row justify="center">
-                        <v-btn color="primary" @click="compareFaces" :disabled="loading">
-                            开始比对
-                        </v-btn>
-                    </v-row>
-                </div>
+                <v-row align="center" justify="space-around">
+                    <v-col cols="12" sm="8" md="6">
+                        <!-- 为图片预览设置最小高度 -->
+                        <image-upload v-model="image1"></image-upload>
+                    </v-col>
+                    <v-col cols="12" sm="8" md="6">
+                        <image-upload v-model="image2"></image-upload>
+                    </v-col>
+                </v-row>
+                <v-row justify="center">
+                    <v-btn color="primary" @click="compareFaces" :disabled="loading">
+                        {{ loading ? '分析中...' : '开始比对' }}
+                    </v-btn>
+                </v-row>
+                <v-row justify="center" v-if="loading">
+                    <v-progress-circular indeterminate color="primary" class="mt-3"></v-progress-circular>
+                </v-row>
                 <!-- 提示信息或比对结果 -->
-                <div v-if="result">
-                    <v-alert :type="resultType">
+                <v-row v-if="result && !loading">
+                    <v-alert :type="resultType" class="mt-3">
                         {{ resultMessage }}
                     </v-alert>
-                </div>
+                </v-row>
             </v-col>
         </v-row>
     </v-container>
@@ -64,12 +65,22 @@ export default {
                         'Content-Type': 'multipart/form-data',
                     },
                 });
+                // 假设 response.data.data 包含比对结果分数
+                const score = parseFloat(response.data.data);
+                let resultText = `相似度分数：${(score*100).toFixed(2)}。\n`;
+
+                if (score > 0.67) {
+                    resultText += '高度相似，极有可能是同一个人。';
+                } else {
+                    resultText += '相似度较低，可能不是同一个人。';
+                }
+
                 this.result = true;
-                this.resultMessage = '比对结果：' + response.data.data;
+                this.resultMessage = resultText;
                 this.resultType = 'success';
             } catch (error) {
                 this.result = true;
-                this.resultMessage = '比对过程中出现错误：' + error.response.data.error;
+                this.resultMessage = '比对过程中出现错误：' + (error.response.data.error || error.message);
                 this.resultType = 'error';
             } finally {
                 this.loading = false;
