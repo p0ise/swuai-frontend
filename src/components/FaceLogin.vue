268f0617920b5a2b3b3abb32e7914366424f5204
p0ise <changelf@163.com>
2024-04-11 18:12:04 +0800

    adjusted the UI and logic of registration and login, and achieved integration with the backend

--- src/components/FaceLogin.vue
+++ src/components/FaceLogin.vue
@@ -1,93 +1,136 @@
 <template>
-  <div class="login-container">
-    <div class="login-box">
-      <h2>欢迎登录</h2>
-      <form @submit.prevent="" class="login-form">
-        <v-row justify="center">
+  <v-container class="login-container">
+    <div class="login-box d-flex justify-center">
+      <v-col class="justify-center" style="max-width: 400px;">
+        <h2>欢迎登录</h2>
+        <v-row justify="center mb-2">
           <div id="videoCanvasWrapper" class="d-flex justify-center position-relative ma-0">
-            <video id="video" ref="video" autoplay playsinline width="400" height="300"></video>
+            <video id="videoElement" ref="videoElement" autoplay playsinline height="300"></video>
             <canvas id="overlay" ref="overlay" width="400" height="300"></canvas>
             <v-overlay id="videoPlaceholder" :model-value="!stream" persistent contained>
-              摄像头未开启，请点击“开启摄像头”按钮。
+              点击“开始登录”开始人脸认证。
             </v-overlay>
           </div>
         </v-row>
-
-        <v-row class="justify-center">
-          <v-col class="d-flex justify-center" style="max-width: 525px;">
-            <v-btn @click="snackbar = true" color="red">test</v-btn>
-
-            <v-snackbar v-model="snackbar" vertical color="success" class="snackbar-center">
-              <div class="text-subtitle-1 pb-2">登录成功！</div>
-
-              <p>用户姓名为: {{name}}</p>
-
-              <template v-slot:actions>
-                <v-btn
-                  color="gray"
-                  variant="text"
-                  @click="snackbar = false"
-                >
-                  关闭
-                </v-btn>
-              </template>
-            </v-snackbar>
-          </v-col>
+        <v-row class="d-flex justify-center">
+          <v-btn color="primary" @click="startLogin" class="mx-2">开始登录</v-btn>
+          <v-btn color="error" @click="cancelLogin" class="mx-2">取消登录</v-btn>
         </v-row>
-        <v-switch class="switch1"
-          v-model="model"
-          color="indigo-darken-3"
-          label="开关摄像头"
-          hide-details
-          inset
-        ></v-switch>
-        <button class="button1" type="submit">登录</button>
-        <p class="text-register">还没有账号？<router-link to="/Register">注册</router-link></p>
-      </form>
+        <p class="text-login">没有账号？<router-link to="/Register">点击注册</router-link></p>
+
+        <v-snackbar v-model="snackbar" :color="snackbarColor">
+          {{ snackbarText }}
+          <v-btn color="white" text @click="snackbar = false">关闭</v-btn>
+        </v-snackbar>
+      </v-col>
     </div>
-  </div>
+  </v-container>
 </template>
 
 <script>
+import io from 'socket.io-client';
+
 export default {
   data() {
     return {
-      video: null,
+      socket: null,
+      videoElement: null,
       overlay: null,
       overlayCtx: null,
-      lastSentTimestamp: Date.now(),
       stream: null,
-      streamingInterval: null,
       snackbar: false,
-      model: false,
-      name: ''
+      snackbarText: '',
+      snackbarColor: 'success',
     };
   },
   mounted() {
-    if (this.model) {
-      this.startCamera();
-    }
-    this.video = this.$refs.video;
+    this.socket = io('ws://localhost:5000/api/face-auth');
+    this.videoElement = this.$refs.videoElement;
     this.overlay = this.$refs.overlay;
     this.overlayCtx = overlay.getContext('2d');
-  },
-  watch: {
-    model(newVal) {
-      if (newVal) {
-        this.startCamera();
-      } else {
-        this.stopCamera();
+
+    this.socket.on('connect', () => {
+      console.log("Connected to Face Recogition");
+    });
+
+    // Listen for server responses
+    this.socket.on('login_response', (data) => {
+      this.snackbarText = data.message;
+      this.snackbarColor = data.success ? 'success' : 'error';
+      this.snackbar = true;
+      if (!data.success && data.message && data.message === "认证失败，未知人脸") {
+        // If the user is unknown, prompt them to register
+        this.snackbarText = "未知用户，请注册";
+        this.snackbarColor = 'info';
       }
-    }
+      if (data.success || data.message === "认证失败，未知人脸") {
+        this.cancelLogin(); // Stop the camera on successful login or prompt to register
+      }
+    });
   },
   methods: {
-    startCamera() {
+    sendFrame() {
+      const currentTimestamp = Date.now();
+      this.lastSentTimestamp = currentTimestamp;
+      const canvas = document.createElement('canvas');
+      canvas.width = this.videoElement.videoWidth;
+      canvas.height = this.videoElement.videoHeight;
+      canvas.getContext('2d').drawImage(this.videoElement, 0, 0, canvas.width, canvas.height);
+      const data = canvas.toDataURL('image/jpeg');
+      if (data && data.length > 0 && !data.endsWith('data:,')) {
+        this.socket.emit('login', { image: data, timestamp: currentTimestamp });
+      } else {
+        console.error("Captured frame is empty or invalid.");
+      }
+    },
+    redraw() {
+      if (this.stream) {
+        // 清除之前的绘制
+        this.overlayCtx.clearRect(0, 0, this.overlay.width, this.overlay.height);
+
+        // 重新绘制所有人脸框和关键点
+        this.lastDetectionResults.forEach(face => {
+          const { id, location, name, prob, landmark } = face;
+          const text = id ? name + "#" + id : name;
+          let [top, right, bottom, left] = location;
+          let width = right - left;
+          let height = bottom - top;
+
+          // 如果当前绘制的框是选中状态，使用特殊颜色或宽度高亮显示
+          if (this.selectedFace && id === this.selectedFace.id) {
+            this.overlayCtx.strokeStyle = 'yellow'; // 高亮颜色
+            this.overlayCtx.lineWidth = 4; // 增加边框宽度以高亮显示
+            this.overlayCtx.fillStyle = 'yellow';
+          } else {
+            this.overlayCtx.strokeStyle = 'red';
+            this.overlayCtx.lineWidth = 2;
+            this.overlayCtx.fillStyle = 'red';
+          }
+          this.overlayCtx.font = '14px Arial';
+
+          // 绘制人脸框
+          this.overlayCtx.strokeRect(left, top, width, height);
+          this.overlayCtx.fillText(text, left, top - 10);
+
+          // 绘制关键点
+          this.overlayCtx.fillStyle = 'blue'; // 关键点的颜色
+          landmark.forEach(point => {
+            // 根据人脸框的缩放调整关键点的位置
+            let [x, y] = point;
+            this.overlayCtx.beginPath();
+            this.overlayCtx.arc(x, y, 2, 0, 2 * Math.PI); // 绘制小圆点标记关键点位置
+            this.overlayCtx.fill();
+          });
+        });
+      }
+    },
+    startLogin() {
       if (this.stream === null) {
         navigator.mediaDevices.getUserMedia({ video: true })
           .then(mediaStream => {
             this.stream = mediaStream;
-            this.video.srcObject = mediaStream;
-            this.video.play();
+            this.videoElement.srcObject = mediaStream;
+            this.videoElement.play();
             this.streamingInterval = setInterval(this.sendFrame, 100);
           })
           .catch(err => {
@@ -95,10 +138,10 @@ export default {
           });
       }
     },
-    stopCamera() {
+    cancelLogin() {
       if (this.stream) {
         this.stream.getTracks().forEach(track => track.stop());
-        this.video.srcObject = null;
+        this.videoElement.srcObject = null;
         this.stream = null;
         clearInterval(this.streamingInterval);
         this.overlayCtx.clearRect(0, 0, this.overlay.width, this.overlay.height);
@@ -109,7 +152,7 @@ export default {
   },
   beforeUnmount() {
     // 清理工作
-    this.stopCamera();
+    this.cancelLogin();
     if (this.socket) {
       this.socket.disconnect();
     }
@@ -149,7 +192,7 @@ h2 {
   border: 5px solid #C0C0C0;
 }
 
-#video,
+#videoElement,
 #overlay {
   position: absolute;
   width: 100%;
@@ -165,69 +208,12 @@ h2 {
   color: #fff;
 }
 
-.input-group {
-  position: relative;
-  margin-bottom: 30px;
-}
-
-.input-group input {
-  width: 100%;
-  padding: 10px 0;
-  background: none;
-  border: none;
-  outline: none;
-  color: #333;
-  font-size: 16px;
-}
-
-.input-group .bar {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 2px;
-  background: #2575fc;
-  transform: scaleX(0);
-  transition: transform 0.3s ease;
-}
-
-.input-group input:focus ~ .bar,
-.input-group input:valid ~ .bar {
-  transform: scaleX(1);
-}
-
-.switch1 {
-  display: block;
-  width: 30%;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-.button1 {
-  display: block;
-  width: 30%;
-  padding: 10px 0;
-  border: none;
-  background: #2575fc;
-  color: white;
-  border-radius: 25px;
-  cursor: pointer;
-  font-size: 18px;
-  transition: background 0.3s ease;
-  margin-left: auto;
-  margin-right: auto;
-}
-
-button:hover {
-  background: #6a11cb;
-}
-
-.text-register {
+.text-login {
   margin-top: 20px;
   text-align: center;
 }
 
-.text-register a {
+.text-login a {
   color: #2575fc;
   text-decoration: none;
 }
